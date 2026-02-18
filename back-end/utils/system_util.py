69538585658f337acc91f3eaa32df861f41eeb6e
Leo-1636 <leo.workroom1@gmail.com>
2026-02-18 10:31:49 +0800

    Update system_util.py

--- back-end/utils/system_util.py
+++ back-end/utils/system_util.py
@@ -3,7 +3,6 @@ from pathlib import Path
 
 from config.config import path as config_path
 
-
 def generate_uuid() -> str:
     return str(uuid.uuid4())
 
@@ -16,7 +15,6 @@ def parse_path(image_path: str) -> tuple[str, str]:
         raise ValueError("Invalid folder")
     return folder, filename
 
-
 def get_image_path(folder: str, filename: str) -> Path:
     if folder == "originals":
         return config_path.original / filename
@@ -24,7 +22,5 @@ def get_image_path(folder: str, filename: str) -> Path:
         return config_path.datasets / filename
     raise ValueError("Invalid folder")
 
-
-def get_txt_path(folder: str, image_filename: str) -> Path:
-    name_without_ext = image_filename.rsplit(".", 1)[0]
-    return get_image_path(folder, f"{name_without_ext}.txt")
+def get_txt_path(folder: str, filename: str) -> Path:
+    return get_image_path(folder, f"{filename.rsplit(".", 1)[0]}.txt")
